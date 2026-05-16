5bbf8437b57ce0a5ba9886f4bf40d9cffc87ecb5
ajstrick81 <adamjstricker@gmail.com>
2026-05-16 18:37:00 -0500

    Add ParamountFingerprint object for ad interactions

---
+++ patches/src/main/kotlin/patches/androidtv/paramount/ParamountFingerprint.kt
@@ -0,0 +1,9 @@
+package patches.androidtv.paramount
+
+import patches.core.ParameterizedFingerprint
+
+object ParamountFingerprint : ParameterizedFingerprint(
+    "invoke-static {.*}, Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;->create\\(Landroid/net/Uri;Ljava/lang/String;Ljava/lang/String;Lcom/google/ads/interactivemedia/v3/internal/zzpm;\\)Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;",
+    "move-result-object (\\w+)",
+    "invoke-virtual {.*}, Lcom/google/ads/interactivemedia/v3/internal/zzew;->zzb\\(Landroid/content/Context;Lcom/google/ads/interactivemedia/v3/impl/data/zzbr;Lcom/google/ads/interactivemedia/v3/impl/data/TestingConfiguration;Ljava/util/concurrent/ExecutorService;\\)Lcom/google/ads/interactivemedia/v3/internal/zzev;"
+)
