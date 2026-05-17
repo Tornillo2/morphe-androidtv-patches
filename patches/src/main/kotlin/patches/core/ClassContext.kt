f700642b61a6b71acdf670b054e180df9dfa9421
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 18:50:25 -0500

    Delete patches/src/main/kotlin/patches directory

--- patches/src/main/kotlin/patches/core/ClassContext.kt
+++
@@ -1,9 +0,0 @@
-package patches.core
-
-class ClassContext(private val className: String) {
-
-    fun forMethod(methodName: String, block: MethodContext.() -> Unit) {
-        val methodContext = MethodContext(methodName)
-        methodContext.block()
-    }
-}
--- patches/src/main/kotlin/patches/core/PatchContext.kt
+++
@@ -1,9 +0,0 @@
-package patches.core
-
-class PatchContext {
-
-    fun forClass(className: String, block: ClassContext.() -> Unit) {
-        val classContext = ClassContext(className)
-        classContext.block()
-    }
-}
