f10d7c7e297d677de31ab5c703f3a741e387f007
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 13:20:48 -0500

    Configure dependency resolution management
    Added dependency resolution management with repositories and version catalogs.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,15 @@
 rootProject.name = "morphe-androidtv-patches"
 
-include(":patches")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
