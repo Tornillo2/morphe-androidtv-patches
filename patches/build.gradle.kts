85433f20ba4d1d7d8fe6599da7bcba1cf38e80b8
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 18:37:12 -0500

    Remove application plugin and related configurations

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,17 +1,8 @@
 plugins {
     kotlin("jvm") version "1.9.22"
-    application
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(libs.morphe.patcher)
-}
-
-application {
-    mainClass.set("patches.MainKt")
 }
