4b0e0a29906b384aaced5f7b97185fe9b7f8dd86
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 21:44:30 -0500

    Enhance build.gradle.kts with project info and tasks
    Added project metadata and new tasks for patch generation.

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,7 +1,56 @@
-plugins {
-    kotlin("jvm") version "1.9.22"
+group = "app.morphe"
+
+patches {
+    about {
+        name = "Morphe Patches"
+        description = "Morphe Patches for Andriod TV."
+        source = "https://github.com/ajstrick81/morphe-androidtv-patches"
+        author = "ajstrick81"
+        contact = "na"
+        website = "https://github.com/ajstrick81/morphe-androidtv-patches"
+        license = "GNU General Public License v3.0, with additional GPL section 7 requirements"
+    }
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
+    compileOnly(libs.morphe.patcher)
+
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+
+    // Required due to smali, or build fails. Can be removed once smali is bumped.
+    implementation(libs.guava)
+
+    // Android API stubs defined here.
+    compileOnly(project(":patches:stub"))
+}
+
+tasks {
+    register<JavaExec>("checkStringResources") {
+        description = "Checks resource strings for invalid formatting"
+
+        dependsOn(compileKotlin)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.resource.CheckStringResourcesKt")
+    }
+
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xcontext-receivers")
+    }
 }
