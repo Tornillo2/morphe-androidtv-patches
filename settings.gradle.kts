9b9808d01f6e2feda978522515f2eee7eee12085
ajstrick81 <adamjstricker@gmail.com>
2026-05-17 19:15:58 -0500

    Add GitHub Maven repository with credentials

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,14 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+
+        maven {
+            url = uri("https://maven.pkg.github.com/MorpheApp/registry")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR") ?: ""
+                password = System.getenv("GITHUB_TOKEN") ?: ""
+            }
+        }
     }
 }
 include(":patches")
