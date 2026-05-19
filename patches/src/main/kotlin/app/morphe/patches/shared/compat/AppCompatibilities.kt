a6450cfdfc489ef645afd69922ef770210150669
ajstrick81 <adamjstricker@gmail.com>
2026-05-18 19:22:34 -0500

    feat: update app names for Paramount and Disney Plus

--- patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
@@ -5,13 +5,14 @@ import app.morphe.patcher.patch.Compatibility
 object AppCompatibilities {
 
     val PARAMOUNT_TV = Compatibility(
-        name = "Paramount+ (Android TV)",
+        name = "Paramount+ for Android TV",
         packageName = "com.cbs.ott",
         appIconColor = 0x0064FF,
+        versions = listOf("16.8.0"),
     )
 
     val DISNEY_PLUS_TV = Compatibility(
-        name = "Disney+ (Android TV)",
+        name = "Disney+ for Android TV",
         packageName = "com.disney.disneyplus",
         appIconColor = 0x113CCF,
     )
