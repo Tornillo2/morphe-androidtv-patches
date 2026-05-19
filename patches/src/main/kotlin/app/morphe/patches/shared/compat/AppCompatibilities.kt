248046d073356aef7a7a166ce3510371ac4e4429
ajstrick81 <adamjstricker@gmail.com>
2026-05-18 19:06:51 -0500

    Refactor AppCompatibilities to use CompatiblePackages

--- patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
+++ patches/src/main/kotlin/app/morphe/patches/shared/compat/AppCompatibilities.kt
@@ -1,26 +1,10 @@
 package app.morphe.patches.shared.compat
 
-import app.morphe.patcher.patch.Compatibility
+import app.morphe.patcher.patch.options.CompatiblePackages
 
-object AppCompatibilities {
-    
-    val PEACOCK_TV = Compatibility(
-        name = "Peacock (Android TV)",
-        packageName = "com.peacocktv.peacockandroid",
-        appIconColor = 0x000000,
-    )
-
+val AppCompatibilities = object {
+    val PEACOCK_TV = CompatiblePackages("com.peacocktv.peacockandroid")
     val PEACOCK_TV_ANDROID_TV = PEACOCK_TV
-
-    val PARAMOUNT_TV = Compatibility(
-        name = "Paramount+ (Android TV)",
-        packageName = "com.cbs.ott",
-        appIconColor = 0x0064FF,
-    )
-
-    val DISNEY_PLUS_TV = Compatibility(
-        name = "Disney+ (Android TV)",
-        packageName = "com.disney.disneyplus",
-        appIconColor = 0x113CCF,
-    )
+    val PARAMOUNT_TV = CompatiblePackages("com.cbs.ott")
+    val DISNEY_PLUS_TV = CompatiblePackages("com.disney.disneyplus")
 }
