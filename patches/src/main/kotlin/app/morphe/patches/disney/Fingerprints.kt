f8cdeadc6aac32da50f692ba098076450b2bae06
ajstrick81 <adamjstricker@gmail.com>
2026-05-19 21:38:16 -0400

    Add Fingerprints for Insertion methods
    This file contains fingerprints for methods 'getPoints' and 'getRanges' in the Insertion class.

---
+++ patches/src/main/kotlin/app/morphe/patches/disney/Fingerprints.kt
@@ -0,0 +1,25 @@
+/*
+ * Forked from:
+ * https://gitlab.com/ReVanced/revanced-patches/-/blob/main/patches/src/main/kotlin/app/revanced/patches/disneyplus/ads/Fingerprints.kt
+ */
+ */ALL CREDIT GOES TO ROOKIEENOUGH FOR THE ORIGINAL CODE
+
+package app.morphe.patches.disney
+
+import app.morphe.patcher.Fingerprint
+
+internal object InsertionGetPointsFingerprint : Fingerprint(
+    returnType = "Ljava/util/List",
+    custom = { method, _ ->
+        method.name == "getPoints" &&
+            method.definingClass == "Lcom/dss/sdk/internal/media/Insertion;"
+    },
+)
+
+internal object InsertionGetRangesFingerprint : Fingerprint(
+    returnType = "Ljava/util/List",
+    custom = { method, _ ->
+        method.name == "getRanges" &&
+            method.definingClass == "Lcom/dss/sdk/internal/media/Insertion;"
+    },
+)
