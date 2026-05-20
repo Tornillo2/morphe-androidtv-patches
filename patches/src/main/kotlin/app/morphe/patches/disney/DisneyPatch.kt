030ecc90d02fd86420940790a1351996be53bfa3
ajstrick81 <adamjstricker@gmail.com>
2026-05-19 21:36:46 -0400

    Refactor DisneyPatch to skip ads automatically

--- patches/src/main/kotlin/app/morphe/patches/disney/DisneyPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/disney/DisneyPatch.kt
@@ -10,41 +10,27 @@
 
 package app.morphe.patches.disney
 
-import app.morphe.patches.shared.compat.AppCompatibilities
-import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
-import app.morphe.patcher.fingerprint
+import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patches.shared.compat.AppCompatibilities
 
 @Suppress("unused")
 val disneyPatch = bytecodePatch(
     name = "Disney+ Android TV",
-    description = "Disables ad insertion in Disney+",
+    description = "Automatically skips ads.",
 ) {
     compatibleWith(AppCompatibilities.DISNEY_PLUS_TV)
 
-    val insertionGetPointsFingerprint = fingerprint {
-        custom { method, _ ->
-            method.definingClass == "Lcom/dss/sdk/internal/media/Insertion;" &&
-                method.name == "getPoints"
-        }
-    }
-
-    val insertionGetRangesFingerprint = fingerprint {
-        custom { method, _ ->
-            method.definingClass == "Lcom/dss/sdk/internal/media/Insertion;" &&
-                method.name == "getRanges"
-        }
-    }
-
     execute {
-        insertionGetPointsFingerprint.method.addInstructions(
-            0,
-            "return-object v0",
-        )
-
-        insertionGetRangesFingerprint.method.addInstructions(
-            0,
-            "return-object v0",
-        )
+        arrayOf(InsertionGetPointsFingerprint, InsertionGetRangesFingerprint).forEach {
+            it.method.addInstructions(
+                0,
+                """
+                    new-instance v0, Ljava/util/ArrayList;
+                    invoke-direct { v0 }, Ljava/util/ArrayList;-><init>()V
+                    return-object v0
+                """.trimIndent(),
+            )
+        }
     }
 }
