ed7ed2744626527d2755ca426d0555fdc37a8b41
ajstrick81 <adamjstricker@gmail.com>
2026-05-24 17:49:29 -0400

    fix: Update returnType in ImaSdkFactoryFingerprint

--- patches/src/main/kotlin/app/morphe/patches/paramount/Fingerprints.kt
+++ patches/src/main/kotlin/app/morphe/patches/paramount/Fingerprints.kt
@@ -3,7 +3,6 @@ package app.morphe.patches.paramount
 import app.morphe.patcher.Fingerprint
 
 internal object ImaSdkFactoryFingerprint : Fingerprint(
-    returnType = "Lcom/google/ads/interactivemedia/v3/api/AdsLoader",
     custom = { method, _ ->
         method.definingClass == "Lcom/google/ads/interactivemedia/v3/api/ImaSdkFactory;" &&
             method.name == "zzb"
