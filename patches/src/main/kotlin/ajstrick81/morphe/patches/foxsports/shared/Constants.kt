6d7c54df5da0909f273bbb3a272695797a15174a
ajstrick81 <adamjstricker@gmail.com>
2026-05-31 19:36:00 -0500

    Update Fox Sports compatibility details

--- patches/src/main/kotlin/ajstrick81/morphe/patches/foxsports/shared/Constants.kt
+++ patches/src/main/kotlin/ajstrick81/morphe/patches/foxsports/shared/Constants.kt
@@ -5,9 +5,9 @@ import app.morphe.patcher.patch.Compatibility
 
 object Constants {
     val COMPATIBILITY = Compatibility(
-        name = "Fox Sports",
+        name = "Fox Sports Android TV",
         packageName = "com.foxsports.android",
-        appIconColor = 0x003767,
+        appIconColor = 0x014990,
         targets = listOf(AppTarget("5.152.0"))
     )
 }
