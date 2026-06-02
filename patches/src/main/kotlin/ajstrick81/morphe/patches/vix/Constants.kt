301e967ed45ef5982cd367fcd0bdae89c1a3c905
ajstrick81 <adamjstricker@gmail.com>
2026-06-02 12:20:42 -0500

    Delete patches/src/main/kotlin/ajstrick81/morphe/patches/vix/Constants.kt

--- patches/src/main/kotlin/ajstrick81/morphe/patches/vix/Constants.kt
+++
@@ -1,6 +0,0 @@
-package ajstrick81.morphe.patches.vix
-
-internal const val PACKAGE_NAME = "com.univision.prendetv"
-internal const val COMPATIBLE_VERSION = "4.46.0_tv"
-
-internal val COMPATIBILITY = PACKAGE_NAME to setOf(COMPATIBLE_VERSION)
