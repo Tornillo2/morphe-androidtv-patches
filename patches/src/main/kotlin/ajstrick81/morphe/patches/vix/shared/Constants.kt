3411cd941817e817fe94385f826e83a65a1589c7
ajstrick81 <adamjstricker@gmail.com>
2026-06-02 17:18:27 -0500

    Refactor Constants for compatibility handling

--- patches/src/main/kotlin/ajstrick81/morphe/patches/vix/shared/Constants.kt
+++ patches/src/main/kotlin/ajstrick81/morphe/patches/vix/shared/Constants.kt
@@ -1,6 +1,13 @@
 package ajstrick81.morphe.patches.vix.shared
 
-internal const val PACKAGE_NAME = "com.univision.prendetv"
-internal const val COMPATIBLE_VERSION = "4.46.0_tv"
+import app.morphe.patcher.patch.AppTarget
+import app.morphe.patcher.patch.Compatibility
 
-internal val COMPATIBILITY = PACKAGE_NAME to setOf(COMPATIBLE_VERSION)
+object Constants {
+    val COMPATIBILITY = Compatibility(
+        name = "ViX Android TV",
+        packageName = "com.univision.prendetv",
+        appIconColor = 0x00A0E9,
+        targets = listOf(AppTarget("4.46.0_tv"))
+    )
+}
