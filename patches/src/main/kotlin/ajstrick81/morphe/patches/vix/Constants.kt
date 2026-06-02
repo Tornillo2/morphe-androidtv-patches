eb5471e0c3e38f16ea5b56d4a96e6d5cc639c50b
ajstrick81 <adamjstricker@gmail.com>
2026-06-02 12:13:01 -0500

    Update package declaration in Constants.kt

--- patches/src/main/kotlin/ajstrick81/morphe/patches/vix/Constants.kt
+++ patches/src/main/kotlin/ajstrick81/morphe/patches/vix/Constants.kt
@@ -1,4 +1,4 @@
-package ajstrick81.morphe.patches.vix.shared
+package ajstrick81.morphe.patches.vix
 
 internal const val PACKAGE_NAME = "com.univision.prendetv"
 internal const val COMPATIBLE_VERSION = "4.46.0_tv"
