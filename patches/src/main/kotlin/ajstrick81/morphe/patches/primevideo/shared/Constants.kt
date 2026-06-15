34ad0a2450e66e005a00d9e0209a16420e89d84b
ajstrick81 <adamjstricker@gmail.com>
2026-06-14 20:06:11 -0500

    Update compatibility name for Prime Video

--- patches/src/main/kotlin/ajstrick81/morphe/patches/primevideo/shared/Constants.kt
+++ patches/src/main/kotlin/ajstrick81/morphe/patches/primevideo/shared/Constants.kt
@@ -5,7 +5,7 @@ import app.morphe.patcher.patch.Compatibility
 
 object Constants {
     val COMPATIBILITY = Compatibility(
-        name = "Prime Video",
+        name = "Prime Video Android TV",
         packageName = "com.amazon.amazonvideo.livingroom",
         appIconColor = 0x177BCE,
         targets = listOf(AppTarget("6.23.23+v15.5.0.70-armv7a"))
